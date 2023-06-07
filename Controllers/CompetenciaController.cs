41f462afa779893c632e4536f92c083abb821623
Franco_mrs <franco.m.silva@aluno.senai.br>
2023-06-07 16:36:07 -0300

    Aula de quarta

--- Controllers/CompetenciaController.cs
+++ Controllers/CompetenciaController.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 
+namespace WebAppnbloom.Controllers;
+
+
+
 
 
 public class CompetenciaController:Controller{
 
+
+
 public IActionResult Index(){
 
+    ViewData["competencia"] = "Entender o funcionamento do Razor";
     return View();
 
 }
--- Models/Competencia.cs
+++ Models/Competencia.cs
@@ -1,6 +1,6 @@
 
 
-
+namespace WebAppnbloom.Models;
 
 
 public class Competencia{
